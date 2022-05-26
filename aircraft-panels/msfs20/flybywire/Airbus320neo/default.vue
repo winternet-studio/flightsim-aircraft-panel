4f681314f5d8a767b910f2b932bcdc8f12cb267c
Allan Jensen <info@winternet.no>
2022-05-26 12:24:47 +0200

    Added iframe for the MCDU

--- aircraft-panels/msfs20/flybywire/Airbus320neo/default.vue
+++ aircraft-panels/msfs20/flybywire/Airbus320neo/default.vue
@@ -6,150 +6,175 @@ Complete API:
 -->
 
 <template>
-	<table align="center" class="std-spacing">
+	<table style="width: 100vw">
 	<tr>
-		<!-- OVERHEAD -->
-		<td style="padding: 0 10px 0 10px">
-
-			<!--
-			DOESN'T CURRENTLY WORK
-			<Toggleable label="10" method="lVar" refName="A320_Neo_MFD_Range" lvarValue="0" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			<Toggleable label="20" method="lVar" refName="A320_Neo_MFD_Range" lvarValue="1" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			<Toggleable label="FUEL" method="hVar" refName="A320_Neo_EICAS_2_ECAM_CHANGE_PAGE_FUEL" lvarValue="1" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			<Toggleable label="ENG" method="hVar" refName="A320_Neo_EICAS_2_ECAM_CHANGE_PAGE_ENG" lvarValue="1" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			-->
-
-			<div class="section">Elec</div>
-			<Toggleable label="BAT" method="offset" refName="batteryMaster" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-
-			<div>&nbsp;</div>
-
-			<Toggleable label="GEN 1" method="offset" refName="genEng1" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			<Toggleable label="APU GEN" method="offset" refName="genApu" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			<Toggleable label="GEN 2" method="offset" refName="genEng2" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-
-			<div style="height: 250px"></div>
-
-			<Toggleable label="STROBE" method="offset" refName="lightsStrobe" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			<Toggleable label="BEACON" method="offset" refName="lightsBeacon" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			<Toggleable label="WING" method="offset" refName="lightsWing" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			<Toggleable label="NAV & LOGO" method="offset" refName="lightsNav" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-
-			<div style="height: 15px"></div>
-
-			<Toggleable label="----" method="" refName="" :dataStore="dataStore" :eventHandlers="eventHandlers" style="opacity: 0" /><!-- just for proper spacing -->
-			<Toggleable label="LANDING LTS" method="offset" refName="lightsLanding" :dataStore="dataStore" :eventHandlers="eventHandlers" style="width: 178px" />
-			<Toggleable label="TAXI" method="offset" refName="lightsTaxi" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-
-		</td>
-		<td style="padding: 0 10px 0 10px">
-			<div class="section">Fuel</div>
-
-			<!-- TODO: some of these are to be Toggleable instead of Indicator -->
-
-			<Indicator label="L BOOST<br/>PUMP" method="offset" refName="fuelPump1" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-			<Indicator label="R BOOST<br/>PUMP" method="offset" refName="fuelPump2" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-
-			<br>
-			<Indicator label="XFLOW" method="offset" refName="fuelXfeed" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-
-			<div class="section">BLEED</div>
-			<Indicator label="SOURCE" method="offset" refName="bleedSrc" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-
-			<div style="height: 15px"></div>
-
-			<Indicator label="APU" method="offset" refName="apuSw" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-			<br>
-			<Indicator label="APU RPM" method="offset" refName="apuRpm" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-			<Indicator label="APU VOLTAGE" method="offset" refName="apuVoltage" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-
-			<div style="height: 15px"></div>
-
-			<Indicator label="ENG L START" method="offset" refName="eng1StarterSw" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-			<Indicator label="ENG R START" method="offset" refName="eng2StarterSw" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-			<SectionDivider />
-			<Indicator label="IGNITION" method="offset" refName="ign" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-
-			<div class="section">Hyd</div>
-			<Indicator label="ENG 1" method="offset" refName="hydPumpEng1" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-			<Indicator label="ENG 2" method="offset" refName="hydPumpEng2" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-
-		</td>
-		<td style="padding: 0 10px 0 10px">
-
-			<div style="height: 190px"></div>
-
-			<div class="section">Anti-Ice</div>
-
-			<Toggleable label="WING" method="offset" refName="antiIceWing" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			<Toggleable label="ENG 1" method="offset" refName="antiIceEng1" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			<Toggleable label="ENG 2" method="offset" refName="antiIceEng2" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-
-			<div style="height: 137px"></div>
-
-			<Toggleable label="SEAT BELT" method="offset" refName="seatBelts" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-
-		</td>
-	</tr>
-	</table>
-
-	<div style="height: 30px"></div>
-
-	<!-- MCP -->
-	<div style="text-align: center">
-
-		<Annunciator label="APU VOLTAGE" method="offset" refName="apuVoltage" :dataStore="dataStore" />
-		<Annunciator label="FD" method="offset" refName="mcpFDSw" :dataStore="dataStore" />
-		<Annunciator label="A/P" method="offset" refName="mcpAPAnnun" :dataStore="dataStore" />
-		<Annunciator label="SPEED" method="offset" refName="mcpSpdAnnun" :dataStore="dataStore" />
-		<Annunciator label="N1" method="offset" refName="mcpN1Annun" :dataStore="dataStore" />
-		<Annunciator label="APPR" method="offset" refName="mcpAppAnnun" :dataStore="dataStore" />
-		<Annunciator label="HDG" method="offset" refName="mcpHdgAnnun" :dataStore="dataStore" />
-		<Annunciator label="NAV" method="offset" refName="mcpNav1Annun" :dataStore="dataStore" />
-		<Annunciator label="ALT" method="offset" refName="mcpAltAnnun" :dataStore="dataStore" />
-
-	</div>
-
-	<div style="height: 30px"></div>
-
-	<!-- PEDESTAL and other -->
-	<table align="center" class="std-spacing" style="width: 100%">
-	<tr>
-		<td></td>
-		<td>
-			<Toggleable label="Spoilers" method="offset" refName="spoilersArm" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-		</td>
-		<td></td>
-		<td></td>
-	</tr>
-	<tr>
-		<td style="width: 35%; text-align: left">
-
-			<Toggleable label="NAV/GPS" method="offset" refName="navGpsSw" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-
-		</td>
 		<td>
 
-			<Indicator label="Speed Break" method="offset" refName="spdBreak" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-			<Toggleable label="Gear" method="offset" refName="gearHandle" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			<br>
-			<Toggleable label="Parking Break" method="offset" refName="parkingBrake" :dataStore="dataStore" :eventHandlers="eventHandlers" />
-			<Indicator label="Flaps" method="offset" refName="flaps" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-			<br>
-			<Indicator label="ENG 1 FUEL" method="offset" refName="eng1CutoffSw" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-			<Indicator label="ENG 2 FUEL" method="offset" refName="eng2CutoffSw" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+
+
+
+			<table align="center" class="std-spacing">
+			<tr>
+				<!-- OVERHEAD -->
+				<td style="padding: 0 10px 0 10px">
+
+					<!--
+					DOESN'T CURRENTLY WORK
+					<Toggleable label="10" method="lVar" refName="A320_Neo_MFD_Range" lvarValue="0" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					<Toggleable label="20" method="lVar" refName="A320_Neo_MFD_Range" lvarValue="1" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					<Toggleable label="FUEL" method="hVar" refName="A320_Neo_EICAS_2_ECAM_CHANGE_PAGE_FUEL" lvarValue="1" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					<Toggleable label="ENG" method="hVar" refName="A320_Neo_EICAS_2_ECAM_CHANGE_PAGE_ENG" lvarValue="1" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					-->
+
+					<div class="section">Elec</div>
+					<Toggleable label="BAT" method="offset" refName="batteryMaster" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+
+					<div>&nbsp;</div>
+
+					<Toggleable label="GEN 1" method="offset" refName="genEng1" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					<Toggleable label="APU GEN" method="offset" refName="genApu" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					<Toggleable label="GEN 2" method="offset" refName="genEng2" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+
+					<div style="height: 250px"></div>
+
+					<Toggleable label="STROBE" method="offset" refName="lightsStrobe" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					<Toggleable label="BEACON" method="offset" refName="lightsBeacon" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					<Toggleable label="WING" method="offset" refName="lightsWing" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					<Toggleable label="NAV & LOGO" method="offset" refName="lightsNav" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+
+					<div style="height: 15px"></div>
+
+					<Toggleable label="----" method="" refName="" :dataStore="dataStore" :eventHandlers="eventHandlers" style="opacity: 0" /><!-- just for proper spacing -->
+					<Toggleable label="LANDING LTS" method="offset" refName="lightsLanding" :dataStore="dataStore" :eventHandlers="eventHandlers" style="width: 178px" />
+					<Toggleable label="TAXI" method="offset" refName="lightsTaxi" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+
+				</td>
+				<td style="padding: 0 10px 0 10px">
+					<div class="section">Fuel</div>
+
+					<!-- TODO: some of these are to be Toggleable instead of Indicator -->
+
+					<Indicator label="L BOOST<br/>PUMP" method="offset" refName="fuelPump1" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+					<Indicator label="R BOOST<br/>PUMP" method="offset" refName="fuelPump2" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+
+					<br>
+					<Indicator label="XFLOW" method="offset" refName="fuelXfeed" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+
+					<div class="section">BLEED</div>
+					<Indicator label="SOURCE" method="offset" refName="bleedSrc" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+
+					<div style="height: 15px"></div>
+
+					<Indicator label="APU" method="offset" refName="apuSw" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+					<br>
+					<Indicator label="APU RPM" method="offset" refName="apuRpm" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+					<Indicator label="APU VOLTAGE" method="offset" refName="apuVoltage" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+
+					<div style="height: 15px"></div>
+
+					<Indicator label="ENG L START" method="offset" refName="eng1StarterSw" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+					<Indicator label="ENG R START" method="offset" refName="eng2StarterSw" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+					<SectionDivider />
+					<Indicator label="IGNITION" method="offset" refName="ign" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+
+					<div class="section">Hyd</div>
+					<Indicator label="ENG 1" method="offset" refName="hydPumpEng1" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+					<Indicator label="ENG 2" method="offset" refName="hydPumpEng2" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+
+				</td>
+				<td style="padding: 0 10px 0 10px">
+
+					<div style="height: 190px"></div>
+
+					<div class="section">Anti-Ice</div>
+
+					<Toggleable label="WING" method="offset" refName="antiIceWing" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					<Toggleable label="ENG 1" method="offset" refName="antiIceEng1" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					<Toggleable label="ENG 2" method="offset" refName="antiIceEng2" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+
+					<div style="height: 137px"></div>
+
+					<Toggleable label="SEAT BELT" method="offset" refName="seatBelts" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+
+				</td>
+			</tr>
+			</table>
+
+			<div style="height: 30px"></div>
+
+			<!-- MCP -->
+			<div style="text-align: center">
+
+				<Annunciator label="APU VOLTAGE" method="offset" refName="apuVoltage" :dataStore="dataStore" />
+				<Annunciator label="FD" method="offset" refName="mcpFDSw" :dataStore="dataStore" />
+				<Annunciator label="A/P" method="offset" refName="mcpAPAnnun" :dataStore="dataStore" />
+				<Annunciator label="SPEED" method="offset" refName="mcpSpdAnnun" :dataStore="dataStore" />
+				<Annunciator label="N1" method="offset" refName="mcpN1Annun" :dataStore="dataStore" />
+				<Annunciator label="APPR" method="offset" refName="mcpAppAnnun" :dataStore="dataStore" />
+				<Annunciator label="HDG" method="offset" refName="mcpHdgAnnun" :dataStore="dataStore" />
+				<Annunciator label="NAV" method="offset" refName="mcpNav1Annun" :dataStore="dataStore" />
+				<Annunciator label="ALT" method="offset" refName="mcpAltAnnun" :dataStore="dataStore" />
+
+			</div>
+
+			<div style="height: 30px"></div>
+
+			<!-- PEDESTAL and other -->
+			<table align="center" class="std-spacing" style="width: 100%">
+			<tr>
+				<td></td>
+				<td>
+					<Toggleable label="Spoilers" method="offset" refName="spoilersArm" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+				</td>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td style="width: 35%; text-align: left">
+
+					<Toggleable label="NAV/GPS" method="offset" refName="navGpsSw" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+
+				</td>
+				<td>
+
+					<Indicator label="Speed Break" method="offset" refName="spdBreak" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+					<Toggleable label="Gear" method="offset" refName="gearHandle" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					<br>
+					<Toggleable label="Parking Break" method="offset" refName="parkingBrake" :dataStore="dataStore" :eventHandlers="eventHandlers" />
+					<Indicator label="Flaps" method="offset" refName="flaps" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+					<br>
+					<Indicator label="ENG 1 FUEL" method="offset" refName="eng1CutoffSw" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+					<Indicator label="ENG 2 FUEL" method="offset" refName="eng2CutoffSw" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+
+				</td>
+				<td style="padding-left: 100px">
+
+					<Indicator label="Trim" method="offset" refName="trim" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+					<br>
+					<Indicator label="-SPACER-" :options="{invisible: true}" :dataStore="dataStore" />
+					<br>
+					<Indicator label="XPNDR" method="offset" refName="sqwkCode" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
+
+				</td>
+				<td style="width: 35%">
+				</td>
+			</tr>
+			</table>
+
+
+
 
 		</td>
-		<td style="padding-left: 100px">
+		<td style="text-align: right">
 
-			<Indicator label="Trim" method="offset" refName="trim" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
-			<br>
-			<Indicator label="-SPACER-" :options="{invisible: true}" :dataStore="dataStore" />
-			<br>
-			<Indicator label="XPNDR" method="offset" refName="sqwkCode" :options="{classes: 'INOPERABLE'}" :dataStore="dataStore" />
 
-		</td>
-		<td style="width: 35%">
+
+
+			<iframe src="http://localhost:8125/sound" style="width: 37vw; height: 100vh"></iframe>
+
+
+
+
 		</td>
 	</tr>
 	</table>
